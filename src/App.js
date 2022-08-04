4a6528e0ce60fcc9e18d348a92abefd60f1ba5ad
jeeva202001 <jeevaece20@gmail.com>
2022-08-05 02:46:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import food from "./emoji-heart-eyes-fill.svg";
+import img from "./img.png";
+const cal = () => {
+	const hide_button = document.getElementById("hide_button");
+	if (hide_button.style.display === "none") {
+		hide_button.style.display = "block";
+	} else {
+		hide_button.style.display = "none";
+	}
+};
+const cal1 = () => {
+	const hide_button = document.getElementById("hide_button1");
+	if (hide_button.style.display === "none") {
+		hide_button.style.display = "block";
+	} else {
+		hide_button.style.display = "none";
+	}
+};
+const cal2 = () => {
+	const hide_button = document.getElementById("hide_button2");
+	if (hide_button.style.display === "none") {
+		hide_button.style.display = "block";
+	} else {
+		hide_button.style.display = "none";
+	}
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id="main_container">
+			<div>
+				<div id="header">
+					<img id="profile" alt="lion" src={img} />
+					<p id="logo_name">Raise</p>
+				</div>
+			</div>
+			<div id="flex">
+				<div id="left_condainer">
+					<div>
+						<div>
+							<button id="sum" onClick={cal}>
+								Food EXP
+							</button>
+						</div>
+						<div id="hide_button">
+							<ul>
+								<li>
+									<button>Break fast</button>
+								</li>
+								<li>
+									<button>lunch</button>
+								</li>
+								<li>
+									<button>dinner</button>
+								</li>
+								<li>
+									<button>Others</button>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div>
+						<div>
+							<button id="sum" onClick={cal1}>
+								<img src={food}></img>TRAVEL EXP
+							</button>
+						</div>
+						<div id="hide_button1">
+							<ul>
+								<li>
+									<button>Train</button>
+								</li>
+								<li>
+									<button>Bus</button>
+								</li>
+								<li>
+									<button>Auto</button>
+								</li>
+								<li>
+									<button>Taxi</button>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div>
+						<div>
+							<button id="sum" onClick={cal2}>
+								Invoices
+							</button>
+						</div>
+						<div id="hide_button2">
+							<ul>
+								<li>
+									<button>Hardware</button>
+								</li>
+								<li>
+									<button>software</button>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="right_condainer"></div>
+			</div>
+		</div>
+	);
 }
-
 export default App;
